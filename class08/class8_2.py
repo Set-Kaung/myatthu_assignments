c06d2b30d25230fd42d565344c0d903cef4f56d0
setkaung <zetaprotocol8@gmail.com>
2024-01-26 20:14:35 +0700

    Added comments for class 8 assignments

--- class08/class8_2.py
+++ class08/class8_2.py
@@ -1,17 +1,22 @@
 nList = [[-2,5,-99,99],[-3,8,1,-2,10],[1,-7,100,-10]]
 
+#the final list of lists
 postiveList = []
 negativeList = []
 
+# the input is a list of list with mixed integers
+# we need to check each element in each list in the list and
+# separate into two lists for each original list
 for i in nList:
-    negatives = []
-    positives = []
-    for j in i: # we can do j in i because i is a list in this case   
+    negatives = [] # negatives for the current list
+    positives = [] # positives for the current list
+
+    for j in i: # we can do j in i because i is a list in this case
         if j < 0:
             negatives.append(j)
         else:
             positives.append(j)
-    
+
     negativeList.append(negatives)
     postiveList.append(positives)
 
